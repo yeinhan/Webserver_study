fb038f76e0522d95fafa68370e2a510e098bd314
=yeinhan <goregore8637@naver.com>
2020-06-30 22:11:03 +0900

    no message

--- jsp02_MultiDelete/src/com/multi/dao/MDBoardDao.java
+++ jsp02_MultiDelete/src/com/multi/dao/MDBoardDao.java
@@ -128,4 +128,104 @@ public class MDBoardDao extends JDBCTemplate {
 		}
 		return res;
 	}
+	
+	public int update(MDBoardDto dto) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res= 0;
+		String sql = " UPDATE MDBOARD SET TITLE=?, CONTENT=? WHERE SEQ=?";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1, dto.getTitle());
+			pstm.setString(2,  dto.getContent());
+			pstm.setInt(3, dto.getSeq());
+			System.out.println("03. query 실행 준비");
+			
+			res = pstm.executeUpdate();
+			System.out.println("04. query 실행");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("3/4 단계  오류");
+			e.printStackTrace();
+		}finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		
+		
+		return res;
+	}
+	
+	public int delete(int seq) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res =0;
+		String sql = " DELETE FROM MDBOARD WHERE SEQ=?";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setInt(1, seq);
+			System.out.println("03. query 준비: "+sql);
+			
+			res=pstm.executeUpdate();
+			
+			if(res>0) {
+				commit(con);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(pstm);
+			close(con);
+			System.out.println(("05. db 종료\n"));
+		}
+		
+		
+		return res;
+	}
+	
+	
+	//다중삭제
+	public int multiDelete(String[] seq) {
+		Connection con = getConnection();
+		PreparedStatement  pstm = null;
+		int res =0;
+		int[] cnt = null;
+		String sql = " DELETE FROM MDBOARD WHERE SEQ=?";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			for(int i=0; i<seq.length; i++) {
+				pstm.setString(1, seq[i]);
+				
+				pstm.addBatch();
+				System.out.println("03. query 준비: " +sql +"(삭제할 번호: "+seq[i]+")");
+			}
+			cnt = pstm.executeBatch();
+			System.out.println("04. query 실행 및 리턴");
+			
+			for(int i=0; i<cnt.length; i++) {
+				if(cnt [i] == -2) {	//성공이면
+					res++;
+				}
+			}
+			if(seq.length == res) {	//우리가 삭제하고자 하는 게시글 번호 seq
+				commit(con);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		
+		return res;
+	}
 }
